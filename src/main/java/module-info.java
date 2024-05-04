7fe04ae6e532a601508c1ce663055936f19558b2
EiderLabiano <elabianbeg@educacion.navarra.es>
2024-05-04 11:56:03 +0200

    prueba commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.gestorempleadoseiderlabiano {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.gestorempleadoseiderlabiano to javafx.fxml;
+    exports com.example.gestorempleadoseiderlabiano;
+}
